fffb1c9de6295cb9474d8f2bd276d464cd9700e3
Lap NGO <van-lap.ngo@cea.fr>
2025-08-11 17:06:20 +0200

    1. 3-component dashboard: demand table, station kpi, power profiles.

--- experimental/demo_dash.py
+++ experimental/demo_dash.py
@@ -4,10 +4,13 @@ from dash import Dash, html, dash_table, dcc
 import pandas as pd
 import cvxpy as cp
 import plotly.express as px
+import dash_bootstrap_components as dbc
 
 from core.planner.day_ahead_planner import create_charging_plans
 from core.utility.data.data_processor import generate_demand_data, prepare_planning_data
 import plotly.graph_objects as go
+
+from core.utility.kpi.eval_performance import compute_energetic_kpi
 
 # %% DATA PREPARATION
 # Meta-parameters
@@ -23,7 +26,7 @@ solver_options_2 = {"solver": cp.SCIPY, "time_limit": 60.0, "verbose": False, "w
 
 data_planning_raw = generate_demand_data(nbr_vehicles=nVE, horizon_length=horizon_length, time_step=time_step)
 data_planning = prepare_planning_data(data_demand=data_planning_raw, time_step=time_step)
-
+data_planning_displayed = data_planning[["powerNom", "energyRequired", "energyMax", "arrivalTime", "departureTime"]]
 
 # %% DAY-AHEAD PLANNING
 
@@ -33,26 +36,73 @@ _, powerProfiles, evcsp = create_charging_plans(
     formulation="milp", solver_options=solver_options_2
 )
 
+kpi_station, kpi_per_ev = compute_energetic_kpi(
+    power_profiles=powerProfiles,
+    power_grid=capacity,
+    planning_input=data_planning,
+    time_step=time_step
+)
+
 # %% Visualization
+# Figure - Power Profiles
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=list(range(horizon_length)), y=powerProfiles.sum(axis=1), name="Total Charging Power (kW)"))
 fig.add_trace(go.Scatter(x=list(range(horizon_length)), y=capacity_grid, name="Infras Capacity (kW)"))
 fig.update_layout(
     title={"text": "Station Power Profiles"},
     xaxis={"title": {"text": "Time Step"}},
-    yaxis={"title": {"text": "Power (kW)"}}
+    yaxis={"title": {"text": "Power (kW)"}},
+    legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01)
+)
+
+#  Fig - KPI
+fig_kpi = go.Figure()
+fig_kpi.add_trace(
+    go.Indicator(
+        mode="number",
+        value=kpi_station["energykWh"],
+        title={'text': "Planned Charging Energy (kWh)"},
+        domain={'row': 0, 'column': 1}
+    )
+)
+fig_kpi.add_trace(
+    go.Indicator(
+        mode="number",
+        value=kpi_station["peakPowerkW"],
+        title={'text': "Peak Power (kW)"},
+        domain={'row': 1, 'column': 1}
+    )
+)
+fig_kpi.update_layout(
+    grid={'rows': 2, 'columns': 1, 'pattern': "independent"}, title={"text": "Station KPIs"},
 )
 
 # %% DASHBOARD APP
-# Initialize the app
-app = Dash()
 
-# App layout
-app.layout = [
-    html.Div(children='Charging Demand Data'),
-    dash_table.DataTable(data=data_planning_raw.to_dict('records'), page_size=10),
-    dcc.Graph(figure=fig)
-]
+app = Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+                html.Div(
+                    className='row', children='Charging Station Supervisor',
+                    style={'textAlign': 'center', 'color': 'red', 'fontSize': 25}
+                )
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dash_table.DataTable(
+                        id="charging-demand-table", data=data_planning_displayed.to_dict('records'),
+                        page_size=10, style_table={'overflowX': 'auto'}
+                    )
+                ),
+                dbc.Col(dcc.Graph(figure=fig_kpi))
+            ]
+        ),
+        dbc.Row(dcc.Graph(figure=fig))
+    ],
+    fluid=True
+)
 
 # Run the app
 if __name__ == '__main__':
