f1de514f1cad7a25d33bde69c093a9bc2b9f14b4
Lap NGO <van-lap.ngo@cea.fr>
2025-08-12 15:23:49 +0200

    1. [dashboard] Add predicting charging demand button

--- core/dashboard/station_supervisor.py
+++ core/dashboard/station_supervisor.py
@@ -1,11 +1,10 @@
 # Import packages
+from typing import List, Dict
 import numpy as np
 from dash import Dash, html, dash_table, dcc, callback, Input, Output
 import pandas as pd
 import cvxpy as cp
-import plotly.express as px
 import dash_bootstrap_components as dbc
-
 from core.dashboard.markups import generate_table, generate_fig_station_power, generate_fig_station_kpi, \
     generate_fig_heatmap_power
 from core.planner.day_ahead_planner import create_charging_plans
@@ -27,9 +26,26 @@ capacity_grid[40:57] = 80
 solver_options_1 = {"solver": cp.CLARABEL, "time_limit": 60.0, "verbose": False, "warm_start": False}
 solver_options_2 = {"solver": cp.SCIPY, "time_limit": 60.0, "verbose": False, "warm_start": False}
 
-data_planning_raw = generate_demand_data(nbr_vehicles=nVE, horizon_length=horizon_length, time_step=time_step)
-data_planning = prepare_planning_data(data_demand=data_planning_raw, time_step=time_step)
-data_planning_displayed = data_planning[["powerNom", "energyRequired", "energyMax", "arrivalTime", "departureTime"]]
+
+@app.callback(
+    [Output(component_id="table-charging-demand", component_property="data")],
+    [Input(component_id="button-charging-demand", component_property="n_clicks")],
+)
+def predict_charging_demand(n_clicks: int = 1) -> List[List[Dict]]:
+    """
+    Predict Charging demand data
+    Notice that returned data for DataTable has to be a list of length 1
+    :param n_clicks:
+    :return:
+    """
+    demand_raw = generate_demand_data(nbr_vehicles=nVE, horizon_length=horizon_length, time_step=time_step)
+    selected_columns = ["powerNom", "energyRequired", "energyMax", "arrivalTime", "departureTime"]
+    return [demand_raw[selected_columns].to_dict("records")]
+
+
+charging_demand = generate_demand_data(nbr_vehicles=nVE, horizon_length=horizon_length, time_step=time_step)
+charging_demand_evcsp = prepare_planning_data(data_demand=charging_demand, time_step=time_step)
+charging_demand_displayed = charging_demand[["powerNom", "energyRequired", "energyMax", "arrivalTime", "departureTime"]]
 
 
 # %% DAY-AHEAD PLANNING
@@ -42,12 +58,14 @@ data_planning_displayed = data_planning[["powerNom", "energyRequired", "energyMa
         Output(component_id="fig-vehicles-powers", component_property="figure")
     ],
     [
-        Input(component_id="charging-demand-table", component_property="data"),
-        Input(component_id="pgrid-slider", component_property="value")
+        Input(component_id="table-charging-demand", component_property="data"),
+        Input(component_id="slider-pgrid", component_property="value")
     ]
 )
-def run_planner(demand, pmax):
-    demand_df = pd.DataFrame.from_records(demand)
+def run_planner(demand: List[Dict], pmax: List | np.array):
+
+    demand_evcsp = prepare_planning_data(data_demand=pd.DataFrame.from_records(demand), time_step=time_step)
+    demand_df = pd.DataFrame.from_records(demand_evcsp)
 
     _, powerProfiles, evcsp = create_charging_plans(
         demand_df, horizon_length=horizon_length, time_step=time_step,
@@ -74,52 +92,17 @@ def run_planner(demand, pmax):
     return fig_power, fig_kpi, fig_vehicle_power
 
 
-# %% Visualization
-# Figure - Power Profiles
-# fig = go.Figure()
-# fig.add_trace(go.Scatter(x=list(range(horizon_length)), y=powerProfiles.sum(axis=1), name="Total Charging Power (kW)"))
-# fig.add_trace(go.Scatter(x=list(range(horizon_length)), y=capacity_grid, name="Infras Capacity (kW)"))
-# fig.update_layout(
-#     xaxis={"title": {"text": "Time Step"}},
-#     yaxis={"title": {"text": "Power (kW)"}},
-#     legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01),
-# )
-
-#  Fig - KPI
-# fig_kpi = go.Figure()
-# fig_kpi.add_trace(
-#     go.Indicator(
-#         mode="number",
-#         value=kpi_station["energykWh"],
-#         title={'text': "Planned Charging Energy (kWh)"},
-#         domain={'row': 0, 'column': 1}
-#     )
-# )
-# fig_kpi.add_trace(
-#     go.Indicator(
-#         mode="number",
-#         value=kpi_station["peakPowerkW"],
-#         title={'text': "Peak Power (kW)"},
-#         domain={'row': 1, 'column': 1}
-#     )
-# )
-# fig_kpi.update_layout(
-#     grid={'rows': 2, 'columns': 1, 'pattern': "independent"}, title={"text": "Station KPIs"}
-# )
-
 # %% DASHBOARD APP
 
 app.layout = dbc.Container(
     [
-        dbc.Row(
-            html.H1("Charging Station Supervisor", className="bg-primary text-white p-1 text-center"),
-        ),
+        dbc.Row([html.H4("Charging Station Supervisor", className="bg-primary text-white p-1 text-center")]),
         dbc.Row(
             [
                 dbc.Col(
                     [
-                        html.H5(children="Charging Demand"),
-                        generate_table(dataframe=data_planning_displayed, id_tag="charging-demand-table")
+                        html.Button(children="Predict Charging Demand", n_clicks=0, id="button-charging-demand"),
+                        generate_table(data=charging_demand_displayed.to_dict("records"), id_tag="table-charging-demand")
                     ]
                 ),
                 dbc.Col(
@@ -128,12 +111,12 @@ app.layout = dbc.Container(
                         dcc.Graph(figure={}, id="fig-station-kpi")
                     ]
                 )
-            ]
+            ],
         ),
         dbc.Row(
             [
                 html.H4(children="Max Power (kW)"),
-                dcc.Slider(id="pgrid-slider", min=50, max=400, value=100, step=20)
+                dcc.Slider(id="slider-pgrid", min=50, max=400, value=100, step=20)
             ]
         ),
         dbc.Row(
@@ -148,7 +131,7 @@ app.layout = dbc.Container(
     fluid=True
 )
 
-# Run the app
+# %% Run the app
 if __name__ == '__main__':
     # run_planner(pmax=capacity_grid)
     app.run(debug=True)
